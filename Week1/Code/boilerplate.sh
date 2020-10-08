65b6746bf9bb987a10aa14f34c45b2e94576bd6d
Olivia Student <o.haas20@imperial.ac.uk>
2020-10-08 14:54:35 +0100

    addedRa new file that we'll remove later

---
+++ Week1/Code/boilerplate.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+# Author: Olivia o.haas20@imperial.ac.uk
+# Script@ boilerplate.sh
+# Desc: simple boilerplate for shell scripts
+# Arguments: none
+# Date: Oct 2020
+
+echo -e "\n This is a shell script! \n"
+
+#exit
