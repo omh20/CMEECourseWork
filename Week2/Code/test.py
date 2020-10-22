2db0348476ed72546cf565a24664fa769692a45c
omh20 <o.haas20@imperial.ac.uk>
2020-10-22 13:37:39 +0100

    week 2 files

---
+++ Week2/Code/test.py
@@ -0,0 +1,7 @@
+first_fasta_file = open('../Data/E.coli.fasta')
+seq1 = first_fasta_file.read('../Data/E.coli.fasta')
+irst_fasta_file.close()
+
+second_fasta_file = open('../Data/407228412.fasta')
+seq2 = second_fasta_file.read()
+second_fasta_file.close()
